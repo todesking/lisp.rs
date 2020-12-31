99759080d954eb5153706b1ac3b437a47ba0e9bd
todesking <discommunicative@gmail.com>
2020-12-31 23:10:32 +0900

    refactor error, prepare cons

--- src/main.rs
+++ src/main.rs
@@ -6,22 +6,24 @@ fn main() -> Result<(), ParseError> {
 
 #[derive(Debug, PartialEq, Eq)]
 enum ParseError {
-    Unexpected(String),
-    Redundant(String),
+    Unexpected,
+    Redundant,
     EOF,
 }
 
 #[derive(Debug, PartialEq, Eq)]
 enum Expr {
     Int(i32),
+    Cons(Box<Expr>, Box<Expr>),
 }
 
 impl std::str::FromStr for Expr {
     type Err = ParseError;
     fn from_str<'a>(s: &'a str) -> Result<Self, Self::Err> {
         let (e, s) = parse_expr(s)?;
+        let s = skip_ws(s);
         if !s.is_empty() {
-            Err(ParseError::Redundant(s.to_owned()))
+            Err(ParseError::Redundant)
         } else {
             Ok(e)
         }
@@ -30,28 +32,46 @@ impl std::str::FromStr for Expr {
 
 type ParseResult<'a> = Result<(Expr, &'a str), ParseError>;
 
-fn parse_expr(s: &str) -> Result<(Expr, &str), ParseError> {
-    match s.chars().next() {
-        Some(c) => match c {
-            '0'..='9' => parse_num(s),
-            _ => Err(ParseError::Unexpected(s.to_owned())),
-        },
-        None => Err(ParseError::EOF),
-    }
-}
-
-fn many(s: &str, mut f: impl FnMut(char) -> bool) -> Option<(&str, &str)> {
+fn many(s: &str, mut f: impl FnMut(char) -> bool) -> (&str, &str) {
     s.char_indices()
         .take_while(|(_, c)| f(*c))
         .last()
         .map(|(i, _)| s.split_at(i + 1))
+        .unwrap_or(("", s))
+}
+
+fn skip_ws(s: &str) -> &str {
+    many(s, |c| c == ' ').1
+}
+
+fn consume<'a>(s: &'a str, pat: &str) -> Option<&'a str> {
+    if s.starts_with(pat) {
+        Some(&s[pat.len()..])
+    } else {
+        None
+    }
+}
+
+
+fn parse_expr(s: &str) -> Result<(Expr, &str), ParseError> {
+    let s = skip_ws(s);
+    parse_num(s).or_else(|_| parse_cons(s))
 }
 
 fn parse_num(s: &str) -> ParseResult {
-    many(s, |c| '0' <= c && c <= '9')
-        .filter(|(s1, _)| !s1.is_empty())
-        .map(|(s1, s2)| (Expr::Int(s1.parse().unwrap()), s2))
-        .ok_or(ParseError::Unexpected(s.to_owned()))
+    let (s1, s2) = many(s, |c| '0' <= c && c <= '9');
+    if s1.is_empty() {
+        Err(ParseError::Unexpected)
+    } else {
+        Ok((Expr::Int(s1.parse().expect("should not reach here")), s2))
+    }
+}
+
+fn parse_cons(s: &str) -> ParseResult {
+    let s = consume(s, "(").ok_or(ParseError::Unexpected)?;
+    let (e1, s) = parse_expr(s)?;
+    let (e2, s) = parse_expr(s)?;
+    Ok((Expr::Cons(Box::new(e1), Box::new(e2)), s))
 }
 
 #[cfg(test)]
@@ -67,10 +87,17 @@ mod test {
         let e = "123".parse();
         assert_eq!(e, Ok(Expr::Int(123)));
 
+        let e = "   123".parse();
+        assert_eq!(e, Ok(Expr::Int(123)));
+
+        let e = "123    ".parse();
+        assert_eq!(e, Ok(Expr::Int(123)));
+
         let e: Result<Expr, ParseError> = "123a".parse();
-        assert_eq!(e, Err(ParseError::Redundant("a".to_owned())));
+        assert_eq!(e, Err(ParseError::Redundant));
 
         let e: Result<Expr, ParseError> = "aaa".parse();
-        assert_eq!(e, Err(ParseError::Unexpected("aaa".to_owned())));
+        assert_eq!(e, Err(ParseError::Unexpected));
+        
     }
 }
