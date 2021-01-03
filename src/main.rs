7e03f61cb4d1a6eb613f7009cca539d9bf43acf6
todesking <discommunicative@gmail.com>
2021-01-03 18:45:20 +0900

    repl

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,13 @@ use io::Write;
 use io::{stdin, stdout};
 use std::io; // for flush()
 
-mod parser;
 mod engine;
+mod parser;
 
 use parser::Expr;
 
-
 fn main() -> io::Result<()> {
+    let env = std::collections::HashMap::<String, engine::Value>::new();
     loop {
         print!("LISP.rs> ");
         stdout().flush()?;
@@ -27,8 +27,24 @@ fn main() -> io::Result<()> {
             break;
         }
 
-        let e = buf.parse::<Expr>();
-        println!("=> {:?}", e);
+        let expr = buf.parse::<Expr>();
+        match expr {
+            Err(err) => {
+                println!("Parse error: {:?}", err);
+            }
+            Ok(expr) => {
+                println!("[Input] {:?}", expr);
+                let v = engine::eval(&expr, &env);
+                match v {
+                    Ok(v) => {
+                        println!("     => {:?}", v);
+                    }
+                    Err(err) => {
+                        println!("Error=> {:?}", err);
+                    }
+                }
+            }
+        }
     }
     Ok(())
 }
