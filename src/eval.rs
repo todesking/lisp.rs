862126fb06d1ae09c9b71e0655e099ade3370293
todesking <discommunicative@gmail.com>
2021-01-03 21:01:26 +0900

    eval: Add data conversion functions

--- src/eval.rs
+++ src/eval.rs
@@ -15,6 +15,43 @@ pub enum Value {
     Cons(Rc<Value>, Rc<Value>),
 }
 
+impl Value {
+    fn cons(car: Value, cdr: Value) -> Value {
+        Value::Cons(Rc::new(car), Rc::new(cdr))
+    }
+}
+
+impl From<i32> for Value {
+    fn from(x: i32) -> Value {
+        Value::Int(x)
+    }
+}
+
+impl<T1, T2> From<(T1, T2)> for Value
+where
+    T1: Into<Value>,
+    T2: Into<Value>,
+{
+    fn from(x: (T1, T2)) -> Value {
+        Value::cons(x.0.into(), x.1.into())
+    }
+}
+
+impl<T: Into<Value>> From<Vec<T>> for Value {
+    fn from(x: Vec<T>) -> Value {
+        x.into_iter()
+            .rev()
+            .fold(Value::Nil, |a, x| Value::cons(x.into(), a))
+    }
+}
+
+#[macro_export]
+macro_rules! list {
+    () =>  { Value::Nil };
+    ($x: expr) => { Value::cons(Value::from($x), Value::Nil) };
+    ($x: expr, $($xs: expr),+) => { Value::cons(Value::from($x), list!($($xs),+)) };
+}
+
 pub type Result = std::result::Result<Value, EvalError>;
 
 pub trait Env {
@@ -68,16 +105,26 @@ mod test {
     }
 
     #[test]
+    fn test_list_macro() {
+        assert_eq!(list!(), Value::Nil);
+        assert_eq!(list!(1), Value::cons(Value::Int(1), Value::Nil));
+        assert_eq!(
+            list!(Value::Int(1), 2),
+            Value::cons(Value::Int(1), Value::cons(Value::Int(2), Value::Nil))
+        );
+    }
+
+    #[test]
     fn test_num() {
         let mut env = new_env();
-        eval_str("1", &mut env).should_ok(Value::Int(1));
+        eval_str("1", &mut env).should_ok(1.into());
     }
 
     #[test]
     fn test_sym() {
         let mut env = new_env();
-        env.insert("x".to_string(), Value::Int(123));
+        env.insert("x".to_string(), 123.into());
 
-        eval_str("x", &mut env).should_ok(Value::Int(123));
+        eval_str("x", &mut env).should_ok(123.into());
     }
 }
