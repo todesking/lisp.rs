b996140e50f1439043f408182ec3af9902b96275
todesking <discommunicative@gmail.com>
2021-01-12 02:53:35 +0900

    cli: add fib function

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,8 @@ use std::io; // for flush()
 
 fn main() -> io::Result<()> {
     let mut global = lisprs::predef();
+    load_cli_env(&mut global);
+
     loop {
         print!("LISP.rs> ");
         stdout().flush()?;
@@ -42,4 +44,21 @@ fn main() -> io::Result<()> {
         }
     }
     Ok(())
+}
+
+fn load_cli_env(global: &mut lisprs::global_env::GlobalEnv) {
+    fn define(global: &mut lisprs::global_env::GlobalEnv, name: &str, src: &str) {
+        let expr = lisprs::parse(src).expect(name);
+        let value = lisprs::eval(&expr, global).expect(name);
+        global.set(name, value);
+    }
+    define(
+        global,
+        "fib",
+        "
+        (lambda (n)
+            (if (eq? n 0) 0
+                (if (eq? n 1) 1
+                    (+ (fib (- n 1)) (fib (- n 2))))))",
+    );
 }
