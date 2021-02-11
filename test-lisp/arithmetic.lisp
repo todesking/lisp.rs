4e3a2455d1cd40af926574225828070cfed9c4f8
todesking <discommunicative@gmail.com>
2021-02-11 21:53:17 +0900

    add tests for arithmetic operators

--- test-lisp/arithmetic.lisp
+++ test-lisp/arithmetic.lisp
@@ -1,3 +1,34 @@
-(assert-eq 1 1)
-
+(assert-error
+  (lambda () (+))
+  '(IllegalArgument))
+(assert-eq (+ 1) 1)
 (assert-eq (+ 1 2) 3)
+
+(assert-error
+  (lambda () (-))
+  '(IllegalArgument))
+(assert-eq (- 1) -1)
+(assert-eq (- 3 8) -5)
+
+(assert-error
+  (lambda () (*))
+  '(IllegalArgument))
+(assert-eq (* 2) 2)
+(assert-eq (* 2 3) 6)
+
+(assert-error
+  (lambda () (/))
+  '(IllegalArgument))
+(assert-error
+  (lambda () (/ 2))
+  '(IllegalArgument 2))
+(assert-eq (/ 5 2) 2)
+
+(assert-error
+  (lambda () (%))
+  '(IllegalArgument))
+(assert-error
+  (lambda () (% 1))
+  '(IllegalArgument 1))
+(assert-eq (% 4 2) 0)
+(assert-eq (% 5 2) 1)
